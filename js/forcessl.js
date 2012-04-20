dcd9725415aa18b0a0485063925ba64bfea3b661
kevdog98 <kevdog98@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-20 20:06:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/verve-ssl/trunk@534256 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/forcessl.js
@@ -0,0 +1,9 @@
+﻿var protocol = location.protocol;
+var hostname = location.hostname;
+var pathname = location.pathname;
+var href = location.href;
+protocol = protocol.toLowerCase();
+hostname = hostname.toLowerCase();
+pathname = pathname.toLowerCase();
+href = href.toLowerCase();
+if (protocol == "http:" ) { protocol = "https:"; location.replace(protocol + "//" + hostname + pathname) } else { };
