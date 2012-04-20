0aa921032cc01d68e3787b597733b9dd2121adea
kevdog98 <kevdog98@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-20 15:38:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/verve-ssl/trunk@534105 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/ssladmincheck.js
@@ -0,0 +1,13 @@
+﻿var protocol = location.protocol;
+var hostname = location.hostname;
+var pathname = location.pathname;
+var href = location.href;
+protocol = protocol.toLowerCase();
+hostname = hostname.toLowerCase();
+pathname = pathname.toLowerCase();
+href = href.toLowerCase();
+var wpAdminMatch = pathname.match(".*wp-admin.*");
+var wpLogin = pathname.match(".*wp-login.php");
+if (protocol == "http:" && pathname == wpAdminMatch) { protocol = "https:"; location.replace(protocol + "//" + hostname + pathname) }
+if (protocol == "http:" && pathname == wpLogin) { protocol = "https:"; location.replace(protocol + "//" + hostname + pathname) }
+else if ((protocol == "https:" && pathname != wpAdminMatch) && (protocol == "https:" && pathname != wpLogin)) { protocol = "http:"; location.replace(protocol + "//" + hostname + pathname) };
