cbfda93273732786537ce5a79243e4a9e936f4d8
kevdog98 <kevdog98@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-23 20:22:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/verve-ssl/trunk@589550 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ noscript.php
@@ -0,0 +1,2 @@
+<meta http-equiv="X-UA-Compatible" content="IE=7.5" ></meta> <!-- IE7 mode -->
+<noscript><center><p>The Verve SSL plugin requires Javascript to be enabled, to learn how to enable Javascript consult your browser documentation.  To login in to Wordpress bypassing this plugin and not using SSL login below.  <B>Please note your login will note be secure<b>.</center></noscript>
